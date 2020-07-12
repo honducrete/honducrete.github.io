10c37b1179e098c2dfdaedfc05c2357a85abafa7
Nelson Díaz <diaznelson23@gmail.com>
2020-07-12 13:34:13 -0600

    Update

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+var name = "";
+var contact = "";
+var message = "";
+const emailHonducrete = "honducrete@gmail.com";
+$( "#btn-send" ).click(function(e) {
+    e.preventDefault();
+    name = $( "#in-name" ).val();
+    contact = $( "#in-contact" ).val();
+    message = $( "#txt-message" ).val();
+    console.log(message);
+    if (!contact){
+        $( "#invalid-in-contact" ).attr("hidden", false);
+    }else{
+        $( "#invalid-in-contact" ).attr("hidden", true);
+        var url=`mailto:${emailHonducrete}?subject=${name} [Website contact]`;
+        $( "#form-contact" ).attr("action", url);
+        $( "#mail-body" ).val(message);
+        $( "#mail-contact" ).val(contact);
+        $( "#form-contact" ).submit();
+    }
+});
