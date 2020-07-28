06e44f66e59b0482c466bbe2f0b6defb465a6fa5
Nelson Díaz <diaznelson23@gmail.com>
2020-07-28 16:36:28 -0600

    [ADD] header logo

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ $( document ).ready(function() {
     if (userLang == "es" || userLang == "es-ES" || userLang == "esES"){
         window.location.href = 'es.html';
     }*/
+    resizeLogo();
+    
 });
+
+$( window ).resize(function() {
+    resizeLogo();
+});
+
+function resizeLogo(){
+    var width = $("#header-name").width();
+    if(width > 350){
+        $("#header-logo").width(350);
+    }else{
+        $("#header-logo").width(width);
+    }
+}
